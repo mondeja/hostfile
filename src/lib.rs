58583c91ac3278f5b507134db353409b9a559de7
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-11-08 18:36:20 +0100

    Fix error

--- src/lib.rs
+++ src/lib.rs
@@ -131,14 +131,14 @@ pub fn parse_file(path: &Path) -> Result<Vec<HostEntry>, String> {
 /// - `/etc/hosts` on Unix.
 /// - `C:\Windows\system32\drivers\etc\hosts` on Windows.
 pub fn parse_hostfile() -> Result<Vec<HostEntry>, String> {
-    parse_file(get_hostfile_path())
+    parse_file(get_hostfile_path()?)
 }
 
 /// Get path to the system hostfile.
-pub fn get_hostfile_path<'a>() -> &'a Path {
+pub fn get_hostfile_path<'a>() -> Result<&'a Path, String> {
     #[cfg(not(windows))]
     {
-        Path::new("/etc/hosts")
+        Ok(Path::new("/etc/hosts"))
     }
 
     #[cfg(windows)]
@@ -174,7 +174,7 @@ pub fn get_hostfile_path<'a>() -> &'a Path {
                 let path_slice = unsafe { slice::from_raw_parts(ptr, wcslen(ptr)) };
                 let os_str: OsString = OsString::from_wide(path_slice);
                 unsafe { CoTaskMemFree(ptr.cast()) };
-                Path::new(&os_str.into_string().unwrap()).join("drivers\\etc\\hosts")
+                Ok(Path::new(&os_str.into_string().unwrap()).join("drivers\\etc\\hosts"))
             }
             _ => {
                 // free any allocated memory even on failure (a null ptr is a no-op for `CoTaskMemFree`)
@@ -396,7 +396,9 @@ mod tests {
 
     #[test]
     fn test_get_hostfile_path() {
-        assert!(get_hostfile_path().exists());
+        let maybe_path = get_hostfile_path();
+        assert!(maybe_path.is_ok());
+        assert!(maybe_path.unwrap().exists());
     }
 
     // The next test only runs on GitHub Actions.
